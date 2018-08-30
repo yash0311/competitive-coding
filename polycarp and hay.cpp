8939d8cff922074197131b6765495cc6e78e82c0
yash0311 <yprajapati0311@gmail.com>
2018-08-30 19:16:57 +0530

    tried

--- polycarp and hay.cpp
+++ polycarp and hay.cpp
@@ -12,11 +12,11 @@ ll visited[MAX][MAX];
 ll dx[]={1,-1,0,0};
 ll dy[]={0,0,1,-1};
 
+
 ll validate(ll row,ll col){
  	if (row>=0 && row<n && col>=0 && col<m) return 1;
  	else return 0;
 }
-
 
 void bfs(ll row,ll col,ll val){
 
@@ -44,10 +44,12 @@ void bfs(ll row,ll col,ll val){
 void bfs2(ll row,ll col,ll val,ll limit){
 	
 	visited[row][col]=1;
+	//completed[row][col]=1;
 	queue<pair<ll,ll> > q;
 	q.push({row,col});
 	count1++;
 	if(count1==limit) return ;
+	//cout<<limit<<endl;
 	while(!q.empty()){
 		pair<ll ,ll> p=q.front();
 		q.pop();
@@ -58,6 +60,8 @@ void bfs2(ll row,ll col,ll val,ll limit){
 			if(validate(x+dx[i],y+dy[i]) && !visited[x+dx[i]][y+dy[i]] && matrix[x+dx[i]][y+dy[i]]>=val){
 				matrix[x+dx[i]][y+dy[i]]=val;
 				visited[x+dx[i]][y+dy[i]]=1;
+				//cout<<x+dx[i]<<" "<<y+dy[i]<<endl;
+				//completed[x+dx[i]][y+dy[i]]=1;
 				q.push({x+dx[i],y+dy[i]});
 				count1++;
 				if(count1==limit) return ;
@@ -69,23 +73,23 @@ void bfs2(ll row,ll col,ll val,ll limit){
 int main()
 {	
 	FIO;
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
 
 	cin>>n>>m>>k;
 	set<ll> st;
-	map<ll,pair<ll,ll> > mp;
+	map<ll,vector<pair<ll,ll> > > mp;
 	for(ll i=0;i<n;i++){
 		for(ll j=0;j<m;j++){
 			cin>>matrix[i][j];
 			st.insert(matrix[i][j]);
-			mp[matrix[i][j]]={i,j};
+			mp[matrix[i][j]].pb({i,j});
 		}
 	}
 	//cout<<"yes"<<endl;
 
 	auto it=st.begin();
-	ll ans=0;
+	ll ans=0,final_row,final_col;
 	for(it=st.begin();it!=st.end();it++){
 		count1=0;
 		ll x=(*it);
@@ -93,28 +97,49 @@ int main()
 		if(k/x > n*m) continue;
 
 		//cout<<x<<endl;
-		pair<ll,ll> p;
-		p= mp[x];
-		ll row=p.first;
-		ll col=p.second;
-		//cout<<row<<" "<<col<<endl;
-		bfs(row,col,x);
-		memset(visited,0,sizeof(visited));
-		if(count1>= k/x) {
+		vector<pair<ll,ll> > var;
+		var=mp[x];
+		
+		//p= mp[x];
+		for(auto i:var){
+			//pair<ll,ll> p;
+			count1=0;
+			ll row=i.first;
+			ll col=i.second;
+			if(!visited[row][col]) {
+				memset(visited,0,sizeof(visited));
+				bfs(row,col,x);
+			}
+			if(count1>= k/x) {
 			ans=x;
+			//cout<<ans<<endl;
+			final_row=row;
+			final_col=col;
 			break;
 		}
+		}	
+
+		
+		//cout<<row<<" "<<col<<endl;
+		
+		//memset(visited,0,sizeof(visited));
+		
 
 	}
-
+	memset(visited,0,sizeof(visited));
 	if(!ans) cout<<"NO"<<endl,exit(0);
 	count1=0;
-	pair<ll,ll> p;
-	p= mp[ans];
-	ll row=p.first;
-	ll col=p.second;
-	bfs2(row,col,ans,k/ans);
+	//cout<<final_row<<" "<<final_col<<endl;
+
+	bfs2(final_row,final_col,ans,k/ans);
 	cout<<"YES"<<endl;
+
+	for(ll i=0;i<n;i++){
+		for(ll j=0;j<m;j++){
+			if(!visited[i][j]) matrix[i][j]=0;
+		}
+	}
+
 	for(ll i=0;i<n;i++){
 		for(ll j=0;j<m;j++){
 			if(matrix[i][j]!=ans) cout<<0<<" ";
